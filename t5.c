fd19bc415da256efd24fd1761fdfae413a660c24
rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2023-11-06 16:39:18 +0530

    Delete t5.c

--- t5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a,b,c,d,e;
-	printf(" enter the value a,b,c,d : ");
-	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-	
-	(a>b )?(a>b)?(a>d)?(a>e)? printf(" a is max "): printf(" e is max"): (d>e)? printf(" d is max "): printf("e is max "):
-	( b>c)? (b>d)?(b>e)?  printf(  " b is max"):printf(" e is max"): (c>d)?(c>e)?printf(" c is max"): printf(" e is max"): (d>e)?printf(" d is max"): printf(" e is max"):; 
