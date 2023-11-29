9aed57ff8305c80eff398a6d8ccd7fdcde603e7d
rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2023-11-29 16:25:58 +0530

    Add files via upload

---
+++ specing/c3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int i,j;
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i; j++)
+		{
+			printf("%d",j);
+			
+		}
+		
+	}
+	
+	for(i=5;i>=1;i--)
+	{
+		for(j=i;j>=1; j--)
+		{
+			printf("%d",j);
+			
+		}
+		printf("\n");
+		
+	}
+}
