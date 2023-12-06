d038efab192522d40cae4a09ee850ab165c544ff
rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2023-12-06 16:32:37 +0530

    Add files via upload

---
+++ 1d/squre.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	
+	int n;
+	printf("enter the size of ");
+	scanf("%d",&n);
+	int a[n];
+	int i;
+	
+	for(i=0;i<n;i++)
+	{
+		printf("enter the value of  a[%d]",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n" ,a[i]*a[i]);
+	}
+}
